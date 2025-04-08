def59f311012527e8b7fdb081e77499ba457a71c
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-08 12:05:38 -0300

    refactor: update home page layout and add global styles with Tailwind and CSS variables

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+import Navegador from '../components/Navegador'
 export default function Inicio() {
-    return  <div>
-    <h1>Fundamentos de Next.Js & React</h1>
-    <h2>Vamos estudar esse framework</h2>
-</div>
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap: "wrap",
+            height: "100vh"
+        }}>
+            <Navegador texto="Estiloso" destino="/estiloso"/>
+            <Navegador texto="Exemplo" destino="/exemplo"/>
+            <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
+        </div>
+    )
     
 }
