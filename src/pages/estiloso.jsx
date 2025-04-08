ef945863096d51fa567b193019a2742301abfd43
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-08 11:56:59 -0300

    feat: create Estiloso page using Layout and CSS Modules

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Estiloso.module.css'
+import Layout from '../components/Layout'
+import Link from 'next/link'
+export default function Estiloso() {
+    return (
+
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                
+                <h1>Estilo usando CSS módulos</h1>
+
+            </div>
+        </Layout>
+
+    )
+}
